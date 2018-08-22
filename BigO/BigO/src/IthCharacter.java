6dbcb48511f7f2a55182daa5b3def81a170137d6
witoldzol <blue3656@gmail.com>
2018-08-22 06:13:19 +0100

    update

---
+++ BigO/BigO/src/IthCharacter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author esu
+ */
+public class IthCharacter {
+    
+    static void ithLetter(){
+       
+//        return (char) ( ( (int) 'a') + i);
+int i = 1;
+        System.out.println((char) ( ( (int) 'a') + i));
+        
+    }
+    
+    public static void main(String[] args) {
+        ithLetter();
+    }
+}
