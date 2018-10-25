77b6f4aab36cc960ab9ffd532664ff3188ad2584
witoldzol <blue3656@gmail.com>
2018-10-25 04:01:11 +0100

    update

---
+++ arrays/1_5/oneWay/src/oneway/Main.java
@@ -0,0 +1,11 @@
+package oneway;
+
+class Main{
+    public static void main(String[] args) {
+        OneWay ow = new OneWay();
+        String a = "pales";
+        String b = "palea";
+        System.out.println(ow.oneAway(a, b));
+                
+    }
+}
--- arrays/1_5/oneWay/src/oneway/OneWay.java
+++ arrays/1_5/oneWay/src/oneway/OneWay.java
@@ -21,9 +21,42 @@ public class OneWay {
     boolean oneAway(String a , String b){
         int lenDiff = lenDifference(a, b);
         if( lenDiff > 1 ) return false;
-        
-        
-        
+        int counter = 0;
+        //different lenghts
+        if( lenDiff == 1){
+            System.out.println("different len");
+            String shorter, longer;
+            int longerIndex =0;
+            if( a.length() > b.length() ){
+                longer = a;
+                shorter = b;
+            } else {
+                longer = b;
+                shorter = a;
+            }
+            for(int i =0; i<shorter.length(); i++, longerIndex++){
+                if( shorter.charAt(i) == longer.charAt(longerIndex) ) continue;
+                longerIndex++;
+                if( shorter.charAt(i) != longer.charAt(longerIndex)) return false;
+                counter++;
+                if(counter > 1 ) return false;
+            }
+            //strings equal
+        } else {
+            System.out.println("same len");
+            for(int i = 0; i< a.length(); i++){
+                if(a.charAt(i)==b.charAt(i)) continue;
+                if(a.charAt(i+1) == b.charAt(i) && b.charAt(i+1)==a.charAt(i)){
+                    counter++;
+                    if(counter>1) return false;
+                } else if(a.charAt(i+1) == b.charAt(i+1)){
+                    counter++;
+                    if(counter> 1) return false;
+                }
+                return false;
+            }
+        }
+        return true;
     }
     
      
