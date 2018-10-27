7b2241fc59d8e8f907398211a6f633c471f9cf3d
witoldzol <blue3656@gmail.com>
2018-10-27 04:58:09 +0100

    update

---
+++ arrays/1_7/rotatematrix/src/src/RotateMatrix.java
@@ -0,0 +1,12 @@
+package src;
+
+class RotateMatrix{
+    
+    void rotate(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int[] is = arr[i];
+            
+        }
+    }
+    
+}
---
+++ codeWars/snippets/src/MultiArray.java
@@ -0,0 +1,13 @@
+class MultiArray{
+    
+    public static void main(String[] args) {
+   
+        int[][] arr = new int[10][5];
+        
+        System.out.println("len "+arr[0].length);
+        
+        for(int i=0; i<arr.length; i++){
+            System.out.println("ok");
+        }
+    }
+}
