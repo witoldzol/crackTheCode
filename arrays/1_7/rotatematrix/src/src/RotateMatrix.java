874b0f69f1b34a6d1ce9bab1d298b53fc451d887
witoldzol <blue3656@gmail.com>
2018-10-27 05:13:26 +0100

    update

--- arrays/1_7/rotatematrix/src/src/RotateMatrix.java
+++ arrays/1_7/rotatematrix/src/src/RotateMatrix.java
@@ -3,10 +3,18 @@ package src;
 class RotateMatrix{
     
     void rotate(int[][] arr){
-        for (int i = 0; i < arr.length; i++) {
-            int[] is = arr[i];
-            
+
+        for(int i=0; i<arr.length; i++){
+
+            for(int j=0; j<arr[i].length; j++){
+                rotatePixel(arr[i][j]);
+                
+            }
         }
+    }
+
+    private int rotatePixel(int i) {
+        return i<<8;
     }
     
 }
--- codeWars/snippets/src/MultiArray.java
+++ codeWars/snippets/src/MultiArray.java
@@ -7,7 +7,11 @@ class MultiArray{
         System.out.println("len "+arr[0].length);
         
         for(int i=0; i<arr.length; i++){
-            System.out.println("ok");
+
+            for(int j=0; j<arr[i].length; j++){
+
+                
+            }
         }
     }
 }
