c9bffa1984f58ffea6b178868817908dac12241a
witoldzol <blue3656@gmail.com>
2018-11-02 05:53:37 +0000

    update

--- patterns/src/decorator/Main.java
+++ patterns/src/decorator/Main.java
@@ -3,7 +3,16 @@ package decorator;
 class Main{
     public static void main(String[] args) {
         Beverage espressoWhip = new Whip(new Espresso());
-        System.out.println("des "+espressoWhip.getDescription());
-        System.out.println("cost "+espressoWhip.cost());
+        Beverage a = new Whip(new Mocha(new HouseBlend()));
+        System.out.println(a.getDescription());
+        System.out.println(a.cost());
+        
+        Beverage amazingLatte = new HouseBlend();
+        amazingLatte = new Whip(amazingLatte);
+        amazingLatte = new Mocha(amazingLatte);
+        amazingLatte = new Mocha(amazingLatte);
+        
+        System.out.println(amazingLatte.getDescription());
+        System.out.println(amazingLatte.cost());
     }
 }
