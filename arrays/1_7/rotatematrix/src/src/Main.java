a6ac4c6990a80907da64a09161dd57715b95c9f2
witoldzol <blue3656@gmail.com>
2018-11-05 05:50:41 +0000

    update

--- arrays/1_7/rotatematrix/src/src/Main.java
+++ arrays/1_7/rotatematrix/src/src/Main.java
@@ -10,5 +10,13 @@ class Main{
         arr = matrix.rotate(arr,0,5);
         System.out.println("=======");
         matrix.printOut(arr);
+        System.out.println("");
+        System.out.println("======= boook solution");
+        
+        Rotate_book rb = new Rotate_book();
+        int[][] arr2 = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};        
+        rb.rotate(arr2);
+        matrix.printOut(arr2);
+        
     }
 }
