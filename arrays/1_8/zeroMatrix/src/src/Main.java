153c386cd8a47117f784257e95e1f1f8963d26f0
witoldzol <blue3656@gmail.com>
2018-11-07 05:46:44 +0000

    update

--- arrays/1_8/zeroMatrix/src/src/Main.java
+++ arrays/1_8/zeroMatrix/src/src/Main.java
@@ -1,11 +1,17 @@
 package src;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import jdk.nashorn.internal.objects.NativeArray;
+
 class Main{
     public static void main(String[] args) {
-        int[][] matrix = {{1,1,1,1},{1,1,0,1},{1,1,1,1},{0,1,1,1},{1,1,1,1}};
+        int[][] matrix = {{1,1,1,1},{1,1,0,1},{1,1,1,1},{0,1,0,1},{1,1,1,1}};
         
         ZeroMatrix zm = new ZeroMatrix();
-        System.out.println( zm.findZeros(matrix) );
+
+        zm.findZeros(matrix);    
         
     }
 }
--- arrays/1_8/zeroMatrix/src/src/ZeroMatrix.java
+++ arrays/1_8/zeroMatrix/src/src/ZeroMatrix.java
@@ -1,27 +1,55 @@
 package src;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class ZeroMatrix{
     //iterate over the whole matrix
-    String findZeros(int[][] matrix){
+    void findZeros(int[][] matrix){
         StringBuilder coordinates = new StringBuilder();
-        
+         
+        Set<Integer> columns = new HashSet<>();
+        boolean foundZero = false;
+        //zero out rows
         for (int i = 0; i < matrix.length; i++) {
-            
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.println(matrix[i][j]);
                 if( matrix[i][j] == 0 ) {
-                    coordinates.append(i);
-                    coordinates.append(j);
+                    columns.add(j);
+                    foundZero = true;
                 }
-                
+                if( foundZero ) Arrays.fill(matrix[i], 0);
+                foundZero = false;
+            }
+        }
+        //zero out columns
+        if( columns.size() > 0 ){
+            for (Integer column : columns) {
+                zeroColumns(matrix, column);
             }
             
         }
-        return coordinates.toString();
+        printMatrix(matrix);
         
     }
 
     
     //iterate again zeroing all rows & columns
-    
+
+    private void zeroColumns(int[][] matrix, Integer column) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][column] = 0;
+        }
+    }
+
+    void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            
+        }
+    }
+
 }
