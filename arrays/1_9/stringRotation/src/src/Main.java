67242ba03f8761468d2bd503d7c5252b056bfd26
witoldzol <blue3656@gmail.com>
2018-11-12 05:47:19 +0000

    update

--- arrays/1_9/stringRotation/src/src/Main.java
+++ arrays/1_9/stringRotation/src/src/Main.java
@@ -1,7 +1,7 @@
 package src;
 class Main{
     public static void main(String[] args) {
-        String s2 = "waterbottlee";
+        String s2 = "waterbottle";
         String s1 = "erbottlewat";
         StringRotation sr = new StringRotation();
         System.out.println(sr.isRotation(s1, s2));
