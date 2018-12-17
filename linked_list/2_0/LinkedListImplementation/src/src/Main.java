980b773583649c3136b08faabff81b20eddd7e34
witoldzol <blue3656@gmail.com>
2018-12-17 04:20:46 +0000

    update

--- linked_list/2_0/LinkedListImplementation/src/src/Main.java
+++ linked_list/2_0/LinkedListImplementation/src/src/Main.java
@@ -19,7 +19,7 @@ public class Main{
         ll.printAll();
         System.out.println("---");
 //        Node a = ll.findFromEnd(ll.getHead(), 7);
-        Node a = ll.getNth(ll.getHead(), 3);
+        Node a = ll.getNth(ll.getHead(), 5);
         System.out.println(a.value);
     }
 }
