07e15a8e245970f584a7e6b4de9cf9bbfccd6368
witoldzol <blue3656@gmail.com>
2019-01-27 08:03:41 +0000

    update

--- linked_list/2_3/src/ll/LinkedList.java
+++ linked_list/2_3/src/ll/LinkedList.java
@@ -2,5 +2,7 @@ package ll;
 
 public interface LinkedList{
     public void add(int value);
-    public 
+    public void remove(int value);
+    public boolean isEmpty();
+    public void printAll();
 }
--- linked_list/2_3/src/ll/MyLinkedList.java
+++ linked_list/2_3/src/ll/MyLinkedList.java
@@ -1,15 +1,44 @@
 package ll;
 
 public class MyLinkedList implements LinkedList{
-
+    
+    Node head;
+    
     @Override
     public void add(int value) {
-        
+        Node n = new Node(value);
+        if(head==null)
+            head = n;
+        else{
+            Node temp = head.next;
+            while(temp!=null){
+                temp = temp.next;
+            }
+            temp = n;
+        }
     }
 
     @Override
     public void remove(int value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(head==null) return;
+        if(head.value==value){
+            head=null;
+            return;
+        }
+        Node previous=head;
+        Node curr = head.next;
+        while(curr!=null){
+            if(curr.value == value){
+                if(curr.next!=null){
+                    previous.next=curr.next;
+                } else {
+                    previous.next = null;
+                    return;
+                }
+            }
+            previous=previous.next;
+            curr=curr.next;
+        }
     }
 
     @Override
